2a9f44dacbde7a8db24e4d08b1aa5dc3ed6834ac
Mahdi Mahjoobi <mahdi.m@email.com>
2026-01-27 20:18:09 +0100

    feat: add coordinate parameter support to Places deep link
    Extended wmf_placesActivityWithURL: to parse and store latitude and
    longitude query parameters. The Places deep link now supports both:
    - Article-based: wikipedia://places/?WMFArticleURL=...
    - Coordinate-based: wikipedia://places/?lat=52.3547498&long=4.8339215
    
    Coordinates are stored in the NSUserActivity userInfo dictionary with
    keys "WMFPlacesLatitude" and "WMFPlacesLongitude" for downstream handling.

--- Wikipedia/Code/NSUserActivity+WMFExtensions.m
+++ Wikipedia/Code/NSUserActivity+WMFExtensions.m
@@ -62,15 +62,31 @@ __attribute__((annotate("returns_localized_nsstring"))) static inline NSString *
 + (instancetype)wmf_placesActivityWithURL:(NSURL *)activityURL {
     NSURLComponents *components = [NSURLComponents componentsWithURL:activityURL resolvingAgainstBaseURL:NO];
     NSURL *articleURL = nil;
+    NSString *latitude = nil;
+    NSString *longitude = nil;
+    
     for (NSURLQueryItem *item in components.queryItems) {
         if ([item.name isEqualToString:@"WMFArticleURL"]) {
             NSString *articleURLString = item.value;
             articleURL = [NSURL URLWithString:articleURLString];
-            break;
+        } else if ([item.name isEqualToString:@"lat"]) {
+            latitude = item.value;
+        } else if ([item.name isEqualToString:@"long"]) {
+            longitude = item.value;
         }
     }
+    
     NSUserActivity *activity = [self wmf_pageActivityWithName:@"Places"];
     activity.webpageURL = articleURL;
+    
+    // Store coordinates in userInfo if both are provided
+    if (latitude && longitude) {
+        NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithDictionary:activity.userInfo ?: @{}];
+        userInfo[@"WMFPlacesLatitude"] = latitude;
+        userInfo[@"WMFPlacesLongitude"] = longitude;
+        activity.userInfo = userInfo;
+    }
+    
     return activity;
 }
 
